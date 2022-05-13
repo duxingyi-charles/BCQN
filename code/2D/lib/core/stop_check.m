544f45e7da23158048ea95adcc73ead4d4f45de4
CharlesDu <dohoney@126.com>
2022-05-13 15:52:03 -0500

    record energy, gradient information when terminated

--- code/2D/lib/core/stop_check.m
+++ code/2D/lib/core/stop_check.m
@@ -4,6 +4,8 @@ function [ output ] = stop_check( un2, un1, grad )
 
 global tol_x_cnt tol_f_cnt stop_cnt tol_x tol_f perimeter_norm
 
+global f_last gnorm_last stopped_type
+
 output = 0;
 
 if norm(un2 - un1) < tol_x * (1 + norm(un1))
@@ -16,6 +18,12 @@ if tol_x_cnt >= stop_cnt
     output = 1; 
     
     stopped_type = 1
+    
+    format long;
+    f_last = energy_value(un2)
+    gnorm_last = norm(grad)
+    perimeter_norm
+    
 end
 
 fn2 = energy_value(un2);
@@ -31,12 +39,24 @@ if tol_f_cnt >= stop_cnt
     output = 1; 
     
     stopped_type = 2
+    
+    format long;
+    f_last = fn2
+    gnorm_last = norm(grad)
+    perimeter_norm
+
 end
 
 if norm(grad) < 1e-3 * perimeter_norm
     output = 1;
     
-    stopped_type = 3    
+    stopped_type = 3
+    
+    format long;
+    f_last = fn2
+    gnorm_last = norm(grad)
+    perimeter_norm
+
 end
 
 end
